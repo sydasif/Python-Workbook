85023a9f5d6a1405151eb9e1e2a3b777927345cc
syed asif <sydasif78@gmail.com>
2022-08-26 10:57:48 +0500

    Add exe03 - faulty cal

---
+++ exe/exe03.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter second number: "))
+
+opt = input("Enter operator? + , -, *, /: ")
+
+pls = '+'
+mns = '-'
+mul = '*'
+div = '/'
+
+if num1 == 45 and num2 == 3 and opt == '*': 
+    print(555)
+elif num1 == 56 and num2 == 9 and opt == '+':
+    print(77)
+elif num1 == 56 and num2 == 4 and opt == '/':
+    print(4)
+elif opt == '+':
+    print(num1 + num2)
+elif opt == '-':
+    print(num1 - num2)
+elif opt == '*':
+    print(num1 * num2)
+elif opt == '/':
+    print(num1 / num2)
+else:
+    print("Error, Try again")
