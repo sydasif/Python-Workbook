22ece028287deb474dce68faca8a125c85c2b777
syed asif <sydasif78@gmail.com>
2022-09-01 16:56:50 +0500

    Add function and changed variables name

--- exe/intermediate/hms.py
+++ exe/intermediate/hms.py
@@ -1,16 +1,15 @@
 # Health Management System
-# 03 Clients Harry, Rohan, Hammad
-# 06 files - 03 for diet and 03 for food
 
-def getdate():
+def date_time():
     from datetime import datetime
     now = datetime.now()
-    t = now.strftime("%m/%d/%Y_%H:%M:%S")
-    return t
+    date_time = now.strftime("%m/%d/%Y_%H:%M:%S")
+    return date_time
 
-dateTime = getdate()
-print(dateTime)
+time_stamp = date_time()
 
+def name(person):
+    return person
 
 print("Well Come to Health Management System...")
 print("What do you want?")
@@ -18,45 +17,47 @@ print("What do you want?")
 print("Read or Write a file?")
 print("Enter [0] for Read and [1] for Write.")
 
-read_or_write = int(input("> "))
+read_write = int(input('> '))
 
-if read_or_write == 0:
+if read_write == 0:
     print("What do you want to retrieve?")
 else:
     print("What do you want to enter?")
 
 print("Enter [0] for Food and [1] for Exercise.")
 
-food_or_exercise = int(input("> "))
+food_or_exercise = int(input('> '))
 
 print("Please enter client name: ")
 print("'Harry', 'Rohan', 'Hammad'")
 
-clientName = input("> ")
+clientName = input('> ').lower()
 
-if clientName.lower() == 'harry' and read_or_write == 0:
+name = name(clientName)
+
+if clientName == name and read_write == 0:
     if food_or_exercise == 0:
-        f = open('harry_food.txt')
+        f = open(f"{name}_food.txt")
         print(f.read())
         f.close()
     else:
-        f = open('harry_exe.txt')
+        f = open(f"{name}_exe.txt")
         print(f.read())
         f.close()
-elif clientName.lower() == 'harry' and read_or_write == 1:
+elif clientName == name and read_write == 1:
     if  food_or_exercise == 0:
         print("Enter food name: ")
-        food_name = input("> ")
-        f = open('harry_food.txt', 'a')
-        content = f"{food_name} {dateTime}\n"
+        food = input('> ')
+        f = open(f"{name}_food.txt", "a")
+        content = f"{food} {time_stamp}\n"
         f.write(content)
         f.close()
     else:
         print("Enter Exercise name: ")
-        exercise_name = input("> ")
-        f = open('harry_exe.txt', 'a')
-        content = f"{exercise_name} {dateTime}\n"
+        exercise = input('> ')
+        f = open(f"{name}_exe.txt", "a")
+        content = f"{exercise} {time_stamp}\n"
         f.write(content)
         f.close()           
 else:
-    print('...........')
+    print("Client does not exist...")
